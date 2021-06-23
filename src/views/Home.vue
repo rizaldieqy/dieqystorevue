c168ac27d62f82db35757b8451949fa9ec3bb671
Dieqy <rizalasshidieqy09@gmail.com>
2021-06-23 14:56:37 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderDieqy />
+    <SectionDieqy />
+    <BannerDieqy />
+    <InstaDieqy />
+    <PartnerDieqy />
+    <FooterDieqy />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderDieqy from '@/components/HeaderDieqy.vue';
+import SectionDieqy from '@/components/SectionDieqy.vue';
+import BannerDieqy from '@/components/BannerDieqy.vue';
+import InstaDieqy from '@/components/InstaDieqy.vue';
+import PartnerDieqy from '@/components/PartnerDieqy.vue';
+import FooterDieqy from '@/components/FooterDieqy.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderDieqy,
+    SectionDieqy,
+    BannerDieqy,
+    InstaDieqy,
+    PartnerDieqy,
+    FooterDieqy
+  }
+}
+</script>
